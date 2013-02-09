f3b2b31acf3ce5027964afdee2b4dcb9124efa7f
Steven Hollidge <stevenhollidge@hotmail.com>
2013-02-09 10:26:30 +0100

    initial commit

---
+++ PivotViewerDemo/AdornerCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls.Pivot;
+
+namespace PivotViewerDemo
+{
+    public class AdornerCommand : IPivotViewerUICommand
+    {
+        private readonly Price price;
+
+        public AdornerCommand(Price price)
+        {
+            this.price = price;
+        }
+
+        #region IPivotViewerUICommand
+
+        public string DisplayName
+        {
+            get { return "Book price"; }
+        }
+
+        public Uri Icon
+        {
+            get { return null; }
+        }
+
+        public object ToolTip
+        {
+            get { return "Click this to book the price"; }
+        }
+
+        #endregion
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            //MessageBox.Show(string.Format("Boom!{0}{0}{1}", Environment.NewLine, price.ToString()));
+            
+            // 1.  Redirect to bbc.co.uk
+            //System.Windows.Browser.HtmlPage.Window.Navigate(new Uri("http://bbc.co.uk", UriKind.Absolute));
+
+            // or
+            // 2.  Use the price underlying as part of the URL 
+            var uriString = string.Format("http://www.google.com/finance?q=NASDAQ:{0}", price.Underlying);
+            var uri = new Uri(uriString, UriKind.Absolute);
+            System.Windows.Browser.HtmlPage.Window.Navigate(uri);
+
+            // or
+            // 3.  Open a new tab (popup)
+            //System.Windows.Browser.HtmlPage.Window.Navigate(uri, "_blank");
+        }
+    }
+}
---
+++ PivotViewerDemo/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Pivot;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace PivotViewerDemo
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.pivotViewer.ItemsSource = Factory.GetData();
+            this.pivotViewer.Loaded += pivotViewer_Loaded;
+        }
+
+        void pivotViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            UIHelper.SetTimeout(5000, () => this.pivotViewer.View = this.pivotViewer.GraphView);
+
+            //this.pivotViewer.Filter = "CouponToday=GE.5_LE.10";
+        }
+
+        private void GetCommands(object sender, PivotViewerCommandsRequestedEventArgs e)
+        {
+            e.Commands.Add(new AdornerCommand(e.Item as Price));
+        }
+    }
+
+    public static class UIHelper
+    {
+        public static void SetTimeout(int milliseconds, Action func)
+        {
+            var timer = new DispatcherTimerContainingAction
+            {
+                Interval = new TimeSpan(0, 0, 0, 0, milliseconds),
+                Action = func
+            };
+            timer.Tick += _onTimeout;
+            timer.Start();
+        }
+
+        private static void _onTimeout(object sender, EventArgs arg)
+        {
+            var t = sender as DispatcherTimerContainingAction;
+            t.Stop();
+            t.Action();
+            t.Tick -= _onTimeout;
+        }
+    }
+
+    public class DispatcherTimerContainingAction : System.Windows.Threading.DispatcherTimer
+    {
+        public Action Action { get; set; }
+    }
+}
